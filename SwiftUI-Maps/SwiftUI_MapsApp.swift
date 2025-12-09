71371ad8456413dd1cff62f6785763ce4c7c5b92
csprasad <codealligator.csprasad@gmail.com>
2025-12-10 00:37:03 +0530

    Initial Commit

---
+++ SwiftUI-Maps/SwiftUI_MapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MapsApp.swift
+//  SwiftUI-Maps
+//
+//  Created by codeAlligator on 10/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
